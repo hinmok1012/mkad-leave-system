356286c3a4d64cea44d1857d9a38ff91a7f91fae
hinmok1012 <moksaihin852@gmail.com>
2025-11-29 00:37:49 +0800

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBK95G2zUxZF_UePoGJLTcfNOuRjrjZUAM",
-  authDomain: "holiday-system-b7e66.firebaseapp.com",
-  projectId: "holiday-system-b7e66",
-  storageBucket: "holiday-system-b7e66.appspot.com",
-  messagingSenderId: "454041427438",
-  appId: "1:454041427438:web:3feb3974ae284c6f5982ff",
-  measurementId: "G-3XPCJ1WEK6",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
