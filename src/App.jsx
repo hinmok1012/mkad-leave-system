fceee5c7124ac6eb5b3c725a3e4d10a7431238b1
hinmok1012 <moksaihin852@gmail.com>
2025-11-30 04:04:40 +0800

    update: change logo to calendar and update app name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { useState, useEffect } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "./firebase";
+
+import Layout from "./components/Layout";   // ⭐ 必須加這行
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import LeaveList from "./components/LeaveList";
+import LeaveForm from "./components/LeaveForm";
+
 
 export default function App() {
   const logout = () => signOut(auth);
